aed4e3e9bcfa4c48f84b76a8022613584e009433
Kite <ixkaito@gmail.com>
2021-03-20 23:47:24 +0900

    exec test

--- bin/wocker.js
+++ bin/wocker.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
+'use strict';
+
 require('../dist/main.js');
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-console.log('hello');
+import { exec } from 'child_process';
+
+exec('docker', (err, stdout, stderr) => {
+  if (err) return;
+  if (stdout) console.log(stdout);
+  if (stderr) console.log(stderr);
+});
