83c31d6f3f6d2769a1511d65ea61fddbb12cc2d8
Kite <ixkaito@gmail.com>
2021-03-21 13:24:49 +0900

    $ npm i -D eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin

---
+++ src/commands/index.ts
@@ -0,0 +1,9 @@
+import run from './run';
+
+type Commands = {
+  [name: string]: (args: any) => void,
+}
+
+const commands: Commands = { run };
+
+export default commands;
---
+++ src/commands/run.ts
@@ -0,0 +1,7 @@
+import yargs from 'yargs'
+
+const run = (args: any) => {
+  console.log(args)
+}
+
+export default run
