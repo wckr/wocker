2889908fcd42259498e2f94faa353fb310579023
Kite <ixkaito@gmail.com>
2021-03-21 12:47:17 +0900

    Test

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,33 @@
-import { exec } from 'child_process';
+// import { exec } from 'child_process';
+import yargs from 'yargs';
+import commands from './commands';
 
-exec('docker', (err, stdout, stderr) => {
-  if (err) return;
-  if (stdout) console.log(stdout);
-  if (stderr) console.log(stderr);
-});
+const argv = yargs(process.argv.slice(2)).argv;
+// console.log(argv);
+
+// const commands = [
+//   'run',
+// ];
+
+const command = process.argv[2];
+
+console.log(command in commands);
+
+
+if (command in commands) {
+  commands[command]({
+    name: 'test',
+  });
+}
+
+// yargs.help("h").alias("h", "help").argv;
+
+// const args = process.argv;
+
+// console.log(args);
+
+// exec('docker', (err, stdout, stderr) => {
+//   if (err) return;
+//   if (stdout) console.log(stdout);
+//   if (stderr) console.log(stderr);
+// });
