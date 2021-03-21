4785aadb7e8a6ae09240de162d9e5c3db53a6247
Kite <ixkaito@gmail.com>
2021-03-21 14:27:11 +0900

    Format test

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,32 @@
-// // import { exec } from 'child_process';
-// import yargs from 'yargs';
-// import commands from './commands';
+// import { exec } from 'child_process';
+import yargs from 'yargs'
+import commands from './commands'
 
-// const argv = yargs(process.argv.slice(2)).argv;
-// // console.log(argv);
+const argv = yargs(process.argv.slice(2)).argv
+// console.log(argv);
 
-// // const commands = [
-// //   'run',
-// // ];
+// const commands = [
+//   'run',
+// ];
 
-// const command = process.argv[2];
+const command = process.argv[2]
 
-// console.log(command in commands);
+console.log(command in commands)
 
-// if (command in commands) {
-//   commands[command]({
-//     name: 'test',
-//   });
-// }
+if (command in commands) {
+  commands[command]({
+    name: 'test',
+  })
+}
 
-// // yargs.help("h").alias("h", "help").argv;
+// yargs.help("h").alias("h", "help").argv;
 
-// // const args = process.argv;
+// const args = process.argv;
 
-// // console.log(args);
+// console.log(args);
 
-// // exec('docker', (err, stdout, stderr) => {
-// //   if (err) return;
-// //   if (stdout) console.log(stdout);
-// //   if (stderr) console.log(stderr);
-// // });
+// exec('docker', (err, stdout, stderr) => {
+//   if (err) return;
+//   if (stdout) console.log(stdout);
+//   if (stderr) console.log(stderr);
+// });
