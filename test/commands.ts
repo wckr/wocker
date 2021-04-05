913ad3de1bb8e34cb1c52b85b18c44cec8dc4ca1
Kite <ixkaito@gmail.com>
2021-04-05 21:49:26 +0900

    Update test

--- test/commands.ts
+++ test/commands.ts
@@ -8,7 +8,7 @@ jest.spyOn(process, 'cwd').mockReturnValue(cwd)
 test('$ wocker run', () => {
   commands.run([])
   expect(exec).toHaveBeenCalledWith(
-    'docker run -d wocker/wordpress',
+    'docker run -d -p 80 -p 3306 -p 8025 wocker/wordpress',
     expect.anything(),
   )
 })
@@ -16,7 +16,15 @@ test('$ wocker run', () => {
 test('$ wocker run --name test', () => {
   commands.run(['--name', 'test'])
   expect(exec).toHaveBeenCalledWith(
-    `docker run -d --name test -v ${cwd}/test wocker/wordpress`,
+    `docker run -d --name test -v ${cwd}/test -p 80 -p 3306 -p 8025 wocker/wordpress`,
+    expect.anything(),
+  )
+})
+
+test('$ wocker run -p 80:80 -p 3306:3306 -p 8025:8025', () => {
+  commands.run(['-p', '80:80', '-p', '3306:3306', '-p', '8025:8025'])
+  expect(exec).toHaveBeenCalledWith(
+    `docker run -d -p 80:80 -p 3306:3306 -p 8025:8025 wocker/wordpress`,
     expect.anything(),
   )
 })
@@ -32,7 +40,7 @@ test('$ wocker run -p 80:80 -p 3306:3306 -p 8025:8025', () => {
 test('$ wocker run nginx', () => {
   commands.run(['nginx'])
   expect(exec).toHaveBeenCalledWith(
-    'docker run -d wocker/wordpress:nginx',
+    'docker run -d -p 80 -p 3306 -p 8025 wocker/wordpress:nginx',
     expect.anything(),
   )
 })
