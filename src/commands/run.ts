07ec383e45c54048ddfc96335f2a038eed64973a
Kite <ixkaito@gmail.com>
2021-04-08 18:57:40 +0900

    Update tests for get-port

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,68 +1,75 @@
 import yargs from 'yargs'
 import { exec } from 'child_process'
 import dockerNames from 'docker-names'
+import getPort from 'get-port'
 
 yargs.command('run', 'Run a new project')
 
 const run = (args: string[]): void | string => {
-  const defaults = {
-    name: dockerNames.getRandomName(),
-    ports: ['80', '3306', '8025'],
-    image: 'wocker/wordpress',
-  }
-
-  const argv = yargs(args).options({
-    name: { type: 'string', default: defaults.name },
-    volume: { type: 'string', alias: 'v' },
-    publish: { alias: 'p', default: defaults.ports },
-  }).argv
-
-  // Container name
-  const name = `--name ${argv.name}`
-
-  // Volume
-  argv.volume = argv.volume || `${process.cwd()}/${argv.name}`
-  const volume = `-v ${argv.volume}:/var/www/wordpress:rw`
-
-  // Publish (ports)
-  if (!(argv.publish instanceof Array)) {
-    argv.publish = [`${argv.publish}`]
-  }
-
-  const ports = argv.publish.map((p) => p.split(':').pop())
-
-  argv.publish.push(...defaults.ports.filter((p) => !ports.includes(p)))
-
-  const publish = argv.publish.reduce((acc: string, p: string) => {
-    return `${acc} -p ${p}`
-  }, '')
-
-  // Image and tag
-  const tag = argv._[0]
-  const image = defaults.image + (tag ? `:${tag}` : '')
-
-  // The whole Docker command
-  const command = `docker run -d ${name} ${volume} ${publish} ${image}`
-    .replace(/\s+/g, ' ')
-    .trim()
-
-  exec(command, (err, stdout, stderr) => {
-    if (err || stderr) {
-      console.log(stderr)
-      return
+  ;(async () => {
+    const defaults = {
+      name: dockerNames.getRandomName(),
+      ports: [
+        `${await getPort()}:80`,
+        `${await getPort()}:3306`,
+        `${await getPort()}:8025`,
+      ],
+      image: 'wocker/wordpress',
     }
 
-    console.log(stdout)
+    const argv = yargs(args).options({
+      name: { type: 'string', default: defaults.name },
+      volume: { type: 'string', alias: 'v' },
+      publish: { alias: 'p', default: defaults.ports },
+    }).argv
 
-    exec(`docker port ${stdout}`, (err, stdout, stderr) => {
+    // Container name
+    const name = `--name ${argv.name}`
+
+    // Volume
+    argv.volume = argv.volume || `${process.cwd()}/${argv.name}`
+    const volume = `-v ${argv.volume}:/var/www/wordpress:rw`
+
+    // Publish (ports)
+    if (!(argv.publish instanceof Array)) {
+      argv.publish = [`${argv.publish}`]
+    }
+
+    const ports = argv.publish.map((p) => p.split(':').pop())
+
+    argv.publish.push(...defaults.ports.filter((p) => !ports.includes(p)))
+
+    const publish = argv.publish.reduce((acc: string, p: string) => {
+      return `${acc} -p ${p}`
+    }, '')
+
+    // Image and tag
+    const tag = argv._[0]
+    const image = defaults.image + (tag ? `:${tag}` : '')
+
+    // The whole Docker command
+    const command = `docker run -d ${name} ${volume} ${publish} ${image}`
+      .replace(/\s+/g, ' ')
+      .trim()
+
+    exec(command, (err, stdout, stderr) => {
       if (err || stderr) {
         console.log(stderr)
         return
       }
 
       console.log(stdout)
+
+      exec(`docker port ${stdout}`, (err, stdout, stderr) => {
+        if (err || stderr) {
+          console.log(stderr)
+          return
+        }
+
+        console.log(stdout)
+      })
     })
-  })
+  })()
 }
 
 export default run
--- test/commands.ts
+++ test/commands.ts
@@ -2,21 +2,28 @@ import commands from '../src/commands'
 import { exec } from 'child_process'
 import dockerNames from 'docker-names'
 
+const defaults = {
+  name: 'foolish_kite',
+  port: '55555',
+  image: 'wocker/wordpress',
+}
+
 const cwd = '/Users/test/wocker'
-const name = 'foolish_kite'
 const docroot = '/var/www/wordpress'
-const publish = '-p 80 -p 3306 -p 8025'
-const image = 'wocker/wordpress'
+const publish = `-p ${defaults.port}:80 -p ${defaults.port}:3306 -p ${defaults.port}:8025`
 
 jest.spyOn(process, 'cwd').mockReturnValue(cwd)
-jest.spyOn(dockerNames, 'getRandomName').mockReturnValue(name)
+jest.spyOn(dockerNames, 'getRandomName').mockReturnValue(defaults.name)
+jest.mock('get-port', () => {
+  return jest.fn(() => defaults.port)
+})
 jest.mock('child_process')
 
 describe('Test the `run` command', () => {
   test('$ wocker run', () => {
     commands.run([])
     expect(exec).toHaveBeenCalledWith(
-      `docker run -d --name ${name} -v ${cwd}/${name}:${docroot}:rw ${publish} ${image}`,
+      `docker run -d --name ${defaults.name} -v ${cwd}/${defaults.name}:${docroot}:rw ${publish} ${defaults.image}`,
       expect.anything(),
     )
   })
@@ -24,7 +31,7 @@ describe('Test the `run` command', () => {
   test('$ wocker run --name test', () => {
     commands.run(['--name', 'test'])
     expect(exec).toHaveBeenCalledWith(
-      `docker run -d --name test -v ${cwd}/test:${docroot}:rw ${publish} ${image}`,
+      `docker run -d --name test -v ${cwd}/test:${docroot}:rw ${publish} ${defaults.image}`,
       expect.anything(),
     )
   })
@@ -32,7 +39,7 @@ describe('Test the `run` command', () => {
   test('$ wocker run --name=test', () => {
     commands.run(['--name=test'])
     expect(exec).toHaveBeenCalledWith(
-      `docker run -d --name test -v ${cwd}/test:${docroot}:rw ${publish} ${image}`,
+      `docker run -d --name test -v ${cwd}/test:${docroot}:rw ${publish} ${defaults.image}`,
       expect.anything(),
     )
   })
@@ -40,7 +47,7 @@ describe('Test the `run` command', () => {
   test('$ wocker run --volume /Users/test/wordpress', () => {
     commands.run(['--volume', '/Users/test/wordpress'])
     expect(exec).toHaveBeenCalledWith(
-      `docker run -d --name ${name} -v /Users/test/wordpress:${docroot}:rw ${publish} ${image}`,
+      `docker run -d --name ${defaults.name} -v /Users/test/wordpress:${docroot}:rw ${publish} ${defaults.image}`,
       expect.anything(),
     )
   })
@@ -48,7 +55,7 @@ describe('Test the `run` command', () => {
   test('$ wocker run -v /Users/test/wordpress', () => {
     commands.run(['-v', '/Users/test/wordpress'])
     expect(exec).toHaveBeenCalledWith(
-      `docker run -d --name ${name} -v /Users/test/wordpress:${docroot}:rw ${publish} ${image}`,
+      `docker run -d --name ${defaults.name} -v /Users/test/wordpress:${docroot}:rw ${publish} ${defaults.image}`,
       expect.anything(),
     )
   })
@@ -56,7 +63,7 @@ describe('Test the `run` command', () => {
   test('$ wocker run -p 80', () => {
     commands.run(['-p', '80'])
     expect(exec).toHaveBeenCalledWith(
-      `docker run -d --name ${name} -v ${cwd}/${name}:${docroot}:rw -p 80 -p 3306 -p 8025 ${image}`,
+      `docker run -d --name ${defaults.name} -v ${cwd}/${defaults.name}:${docroot}:rw -p 80 -p 3306 -p 8025 ${defaults.image}`,
       expect.anything(),
     )
   })
@@ -64,7 +71,7 @@ describe('Test the `run` command', () => {
   test('$ wocker run -p 8888:80', () => {
     commands.run(['-p', '8888:80'])
     expect(exec).toHaveBeenCalledWith(
-      `docker run -d --name ${name} -v ${cwd}/${name}:${docroot}:rw -p 8888:80 -p 3306 -p 8025 ${image}`,
+      `docker run -d --name ${defaults.name} -v ${cwd}/${defaults.name}:${docroot}:rw -p 8888:80 -p 3306 -p 8025 ${defaults.image}`,
       expect.anything(),
     )
   })
@@ -72,7 +79,7 @@ describe('Test the `run` command', () => {
   test('$ wocker run -p 8888:80 --publish=8889:3306', () => {
     commands.run(['-p', '8888:80', '--publish=8889:3306'])
     expect(exec).toHaveBeenCalledWith(
-      `docker run -d --name ${name} -v ${cwd}/${name}:${docroot}:rw -p 8888:80 -p 8889:3306 -p 8025 ${image}`,
+      `docker run -d --name ${defaults.name} -v ${cwd}/${defaults.name}:${docroot}:rw -p 8888:80 -p 8889:3306 -p 8025 ${defaults.image}`,
       expect.anything(),
     )
   })
@@ -80,7 +87,7 @@ describe('Test the `run` command', () => {
   test('$ wocker run -p 8888:80 -p 8889:3306 -p 8890:443', () => {
     commands.run(['-p', '8888:80', '-p', '8889:3306', '-p', '8890:443'])
     expect(exec).toHaveBeenCalledWith(
-      `docker run -d --name ${name} -v ${cwd}/${name}:${docroot}:rw -p 8888:80 -p 8889:3306 -p 8890:443 -p 8025 ${image}`,
+      `docker run -d --name ${defaults.name} -v ${cwd}/${defaults.name}:${docroot}:rw -p 8888:80 -p 8889:3306 -p 8890:443 -p 8025 ${defaults.image}`,
       expect.anything(),
     )
   })
@@ -88,7 +95,7 @@ describe('Test the `run` command', () => {
   test('$ wocker run nginx', () => {
     commands.run(['nginx'])
     expect(exec).toHaveBeenCalledWith(
-      `docker run -d --name ${name} -v ${cwd}/${name}:${docroot}:rw ${publish} ${image}:nginx`,
+      `docker run -d --name ${defaults.name} -v ${cwd}/${defaults.name}:${docroot}:rw ${publish} ${defaults.image}:nginx`,
       expect.anything(),
     )
   })
@@ -96,7 +103,7 @@ describe('Test the `run` command', () => {
   test('$ wocker run nginx pwd', () => {
     commands.run(['bash'])
     expect(exec).toHaveBeenCalledWith(
-      `docker run -d --name ${name} -v ${cwd}/${name}:${docroot}:rw ${publish} ${image}:nginx`,
+      `docker run -d --name ${defaults.name} -v ${cwd}/${defaults.name}:${docroot}:rw ${publish} ${defaults.image}:nginx`,
       expect.anything(),
     )
   })
