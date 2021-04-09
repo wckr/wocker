9420dd8741bf7e57a5974a933f60a8926d15f104
Kite <ixkaito@gmail.com>
2021-04-09 17:27:28 +0900

    Refactoring

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -5,69 +5,67 @@ import getPort from 'get-port'
 
 yargs.command('run', 'Run a new project')
 
-const run = (args: string[]): void | string => {
-  ;(async () => {
-    const defaults = {
-      name: dockerNames.getRandomName(),
-      ports: ['80', '3306', '8025'],
-      image: 'wocker/wordpress',
+const run = async (args: string[]): Promise<void> => {
+  const defaults = {
+    name: dockerNames.getRandomName(),
+    ports: ['80', '3306', '8025'],
+    image: 'wocker/wordpress',
+  }
+
+  const argv = yargs(args).options({
+    name: { type: 'string', default: defaults.name },
+    volume: { type: 'string', alias: 'v' },
+    publish: { alias: 'p', default: [''] },
+  }).argv
+
+  // Container name
+  const name = `--name ${argv.name}`
+
+  // Volume
+  argv.volume = argv.volume || `${process.cwd()}/${argv.name}`
+  const volume = `-v ${argv.volume}:/var/www/wordpress:rw`
+
+  // Image and tag
+  const tag = argv._[0]
+  const image = defaults.image + (tag ? `:${tag}` : '')
+
+  // Publish (ports)
+  if (!(argv.publish instanceof Array)) {
+    argv.publish = [`${argv.publish}`]
+  }
+
+  let ports = defaults.ports.filter(
+    (p) => !argv.publish.map((p) => p.split(':').pop()).includes(p),
+  )
+  ports = await Promise.all(ports.map((p) => `${getPort()}:${p}`))
+  argv.publish.push(...ports)
+
+  const publish = argv.publish.reduce((acc: string, p: string) => {
+    return `${acc} -p ${p}`
+  }, '')
+
+  // The whole Docker command
+  const command = `docker run -d ${name} ${volume} ${publish} ${image}`
+    .replace(/\s+/g, ' ')
+    .trim()
+
+  exec(command, (err, stdout, stderr) => {
+    if (err || stderr) {
+      console.log(stderr)
+      return
     }
 
-    const argv = yargs(args).options({
-      name: { type: 'string', default: defaults.name },
-      volume: { type: 'string', alias: 'v' },
-      publish: { alias: 'p', default: [''] },
-    }).argv
+    console.log(stdout)
 
-    // Container name
-    const name = `--name ${argv.name}`
+    // exec(`docker port ${stdout}`, (err, stdout, stderr) => {
+    //   if (err || stderr) {
+    //     console.log(stderr)
+    //     return
+    //   }
 
-    // Volume
-    argv.volume = argv.volume || `${process.cwd()}/${argv.name}`
-    const volume = `-v ${argv.volume}:/var/www/wordpress:rw`
-
-    // Image and tag
-    const tag = argv._[0]
-    const image = defaults.image + (tag ? `:${tag}` : '')
-
-    // Publish (ports)
-    if (!(argv.publish instanceof Array)) {
-      argv.publish = [`${argv.publish}`]
-    }
-
-    let ports = defaults.ports.filter(
-      (p) => !argv.publish.map((p) => p.split(':').pop()).includes(p),
-    )
-    ports = await Promise.all(ports.map((p) => `${getPort()}:${p}`))
-    argv.publish.push(...ports)
-
-    const publish = argv.publish.reduce((acc: string, p: string) => {
-      return `${acc} -p ${p}`
-    }, '')
-
-    // The whole Docker command
-    const command = `docker run -d ${name} ${volume} ${publish} ${image}`
-      .replace(/\s+/g, ' ')
-      .trim()
-
-    exec(command, (err, stdout, stderr) => {
-      if (err || stderr) {
-        console.log(stderr)
-        return
-      }
-
-      console.log(stdout)
-
-      // exec(`docker port ${stdout}`, (err, stdout, stderr) => {
-      //   if (err || stderr) {
-      //     console.log(stderr)
-      //     return
-      //   }
-
-      //   console.log(stdout)
-      // })
-    })
-  })()
+    //   console.log(stdout)
+    // })
+  })
 }
 
 export default run
